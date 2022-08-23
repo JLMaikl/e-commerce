5552c6ebea562bdc8d6765cbb72da25f8d66c6e2
JLMaikl <jlmaikl@mail.ru>
2022-08-23 09:28:01 +0300

    Начинает 3 дз

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
+import axios from "axios";
+
 import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const result = await axios({
+        method: "get",
+        url: "https://fakestoreapi.com/products",
+      })
+      setUsers(result.data);
+    }
+    fetch()
+  }, []);
+
   return (
     <div className="App">
       srtydrtyeryerdtyudtyururu
